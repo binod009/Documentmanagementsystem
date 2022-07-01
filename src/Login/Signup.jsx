3b88f0130925afaddec8d1456ee5c22eb2cc0638
binod009 <ramguy007@gmail.com>
2022-07-01 12:25:38 +0545

    latestupdate

--- src/Login/Signup.jsx
+++ src/Login/Signup.jsx
@@ -1,77 +1,183 @@
-import React, { useState,useRef } from "react";
+import React, { useState,useRef,useEffect } from "react";
 import "./signup.css";
 import logo from '../../src/logo11.png';
-import {Link} from 'react-router-dom';
 import { TextField, Button, Card, CardContent, Grid, Typography,Avatar,Box} from "@mui/material";
 import {AuthContext} from '../context/AuthContext';
+import Errors from "../Errors/Errors";
+import { createUserWithEmailAndPassword} from 'firebase/auth';
+import {auth} from '../firebase';
+import {db} from '../firebase';
+import {doc, setDoc} from "firebase/firestore";
+import {Link, useNavigate} from 'react-router-dom';
+export default function Signup() {
+  const navigate = useNavigate();
+  const [register,setRegister]  = useState({
+    firstname:'',
+    lastname:'',
+    phone:null,
+    email:'',
+    address:'',
+    password:'',
+  });
+  const [isSubmit,setIsSubmit] = useState(false);
+  const[formErrors,setFormErrors]= useState(register);
 
-export default function () {
+const handleInput =(e)=>{
+e.preventDefault();
+const id = e.target.id;
+const value = e.target.value;
+setRegister({...register,[id]:value});
+}
+
+const validate = (values)=>{
+const errors={};
+const regex =/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i ;
+const specialChars = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,}$/;
+if(!values.firstname){
+  errors.firstname="Enter your FirstName";
+}if(!values.lastname){
+  errors.lastname="Enter your LastName"
+}if(!values.email){
+  errors.email = "Enter your email";
+}else if(!regex.test(values.email)){
+  errors.email="Enter a valid Email";
+}
+if(!values.address){
+  errors.address="Enter your address"
+}
+if(values.phone===null){
+errors.phone="Enter your phonenumber"
+}if(!values.password){
+  errors.password="Enter your Password";
+}else if(values.password.length<=4){
+  errors.password="Password must be more than 4 Character";
+}else if(!specialChars.test(values.password)){
+  errors.password = "Password must container special character @$!*"
+}
+return errors;
+}
+
+const handleSubmit=(e)=>{
+  e.preventDefault();
+  setIsSubmit(true);
+  setFormErrors(validate(register));
+}
+
+useEffect(() => {
+if(Object.keys(formErrors).length === 0 && isSubmit){
+ createUserWithEmailAndPassword(auth, register.email, register.password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    setDoc(doc(db,"admin",user.uid),{
+      id:user.uid,
+      name:register.firstname,
+      lastname:register.lastname,
+      email:register.email,
+      phone:register.phone,
+      address:register.address,
+    });
+     navigate('/Login');
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+}
+},[formErrors]);
 
   return (
     <>
-        <Card style={{ backgroundColor:"#eee" ,maxWidth:550,height:350 ,margin:"6rem auto",padding:"10px 15px"}}>
+      <Card style={{ backgroundColor:"#eee" ,maxWidth:580,height:370 ,margin:"6rem auto",padding:"10px 15px"}}>
           <CardContent >
         <Box display="flex" alignitems="center" justifyContent="center"><Avatar  align="center" src={logo} sx={{width:45,height:45}}></Avatar></Box>
             <Typography style={{fontSize:"14px",fontWeight:"700",color:"darkblue",margin:"0.5rem"}}align="center" color="#444" gutterBottom variant="h5">SIGN UP</Typography>
-          <form >
+          <form onSubmit={handleSubmit}>
             <Grid container spacing={2}>
               <Grid xs={12} sm={6} item>
                 <TextField
-                  label="FullName"
+                  label="Firstname"
                   fullWidth
+                  id="firstname"
                   size="small"
+                  onChange={handleInput}
+                  value={register.firstname}
                   variant="outlined"
-                 
                 ></TextField>
+                <Errors message={formErrors.firstname}/>
               </Grid>
               <Grid xs={12} sm={6} item>
                 <TextField
-                  label="Username"
+                  id="lastname"
+                  label="lastname"
+                  onChange={handleInput}
                   fullWidth
                   size="small"
+                  value={register.lastname}
                   variant="outlined"
                   
                 ></TextField>
+                <Errors message={formErrors.lastname}/>
+
               </Grid>
               <Grid xs={12} sm={6} item>
                 <TextField
+                id="address"
+                onChange={handleInput}
                   label="Address"
                   fullWidth
+                  value={register.address}
                   size="small"
                   variant="outlined"
                  
                 ></TextField>
+                <Errors message={formErrors.address}/>
+
               </Grid>
               <Grid xs={12} sm={6} item>
                 <TextField
+                 id="phone"
                   type="number"
+                  value={register.phone}
+                  onChange={handleInput}
                   label="Phone"
                   fullWidth
                   size="small"
                   variant="outlined"
                  
                 ></TextField>
+                <Errors message={formErrors.phone}/>
+
               </Grid>
               <Grid xs={12} sm={6} item>
                 <TextField
-                  type="email"
+                id="email"
+                  type="text"
+                  value={register.email}
                   label="Email"
+                  onChange={handleInput}
                   fullWidth
                   size="small"
-                  variant="outlined"
-             
-          
+                  variant="outlined"      
                 ></TextField>
+                <Errors message={formErrors.email}/>
+
               </Grid>
               <Grid xs={12} sm={6} item>
                 <TextField
+                type="password"
+                id="password"
+                value={register.password}
                   label="Password"
                   fullWidth
+                  onChange={handleInput}
                   size="small"
                   variant="outlined"
-               
-             
+                    autoComplete="false"
                 ></TextField>
+                <Errors message={formErrors.password}/>
+
               </Grid>
               <Grid xs={12}item>
 
--- src/Pages/user/User.jsx
+++ src/Pages/user/User.jsx
@@ -1,9 +1,115 @@
 import React from 'react'
 import './user.css';
+import{db} from '../../firebase';
+import{addDoc, doc,getDoc, updateDoc} from "firebase/firestore";
+import {useParams} from 'react-router-dom';
 import SideBar from '../../Admin/SideBar';
 import TopBar from '../../Admin/Topbar';
+import {getStorage,ref,uploadBytesResumable,getDownloadURL} from 'firebase/storage';
 import {PermIdentity,MyLocationOutlined,Male,AlternateEmail,WorkOutline, PhoneAndroidOutlined, PublishOutlined} from '@mui/icons-material';
+import { useEffect,useState } from 'react';
+import Progress from '../../Loader/Progress';
+import { setPersistence } from 'firebase/auth';
 export default function User() {
+const {userid} = useParams();
+const [file,setFile] = useState("");
+const [open,setOpen]= useState(false);
+const [issubmit,setIsSubmit] = useState(false);
+let newurl ="";
+const [edituser,setEditUser]=useState({
+    fullname:"",
+    post:"",
+    email:"",
+    phone:"",
+    address:"",
+    img:"",
+    age:""
+});
+
+const update = async(newurl)=>{
+if(newurl){
+  const userRef = doc(db,"users",userid);
+  await updateDoc(userRef,{
+    ...edituser
+  });
+  setEditUser({fullname:"",phone:"",address:"",phone:"",img:"",age:""});
+}else{
+  const userRef = doc(db,"users",userid)
+    await updateDoc(userRef,{
+      ...edituser,img:newurl
+  })
+  setEditUser({fullname:"",phone:"",address:"",phone:"",img:"",age:""});
+  }
+}
+
+
+const handleSubmit = async(newurl)=>{
+if(newurl){
+update(newurl);
+}
+update();
+}
+useEffect(()=>{
+const upload=()=>{
+  setOpen(!open);
+  const storage = getStorage();
+    const name = new Date().getTime() + file.name;
+    const storageRef = ref(storage,name);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    // Register three observers:
+    // 1. 'state_changed' observer, called any time the state changes
+    // 2. Error observer, called on failure
+    // 3. Completion observer, called on successful completion
+    uploadTask.on('state_changed', 
+      (snapshot) => {
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+       
+        switch (snapshot.state) {
+          case 'paused':
+            console.log('Upload is paused');
+            break;
+          case 'running':
+            console.log('Upload is running');
+            break;
+        }
+      }, 
+      (error) => {
+        
+      }, 
+      () => {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+           newurl = downloadURL;
+        });
+        setOpen(false);
+})
+}
+file && upload();
+},[file])
+useEffect(()=>{
+const changeimage=()=>{
+setEditUser({...edituser,img:URL.createObjectURL(file)});
+}
+file && changeimage();
+},[file]);
+useEffect(()=>{
+   userid && getSingleuser();
+  },[userid])
+const getSingleuser=async()=>{
+const docRef = doc(db,'users',userid);
+const snapshot = await getDoc(docRef);
+if(snapshot.exists()){
+    setEditUser({...snapshot.data()}); 
+   }
+}
+const handleInput=(e)=>{
+const id = e.target.id;
+const value = e.target.value;
+setEditUser({...edituser,[id]:value});
+}
   return (
     <div className='user'> 
     <div className='sidebar-container'>
@@ -17,39 +123,39 @@ export default function User() {
 <div className='edituser-container'>
    <div className='userShow'>
   <div className='userShowTop'>
-    <img className="userShowImg" src="https://c4.wallpaperflare.com/wallpaper/260/928/20/6k-portrait-ariana-grande-wallpaper-preview.jpg" alt="" />
+    <img className="userShowImg" src={edituser.img} alt="profile" />
     <div className='userShowTopTitle'>
-        <span className='usershowUsername'>Binod khatri</span>
-        <span className='usershowPost'>Manager</span>
+        <span className='usershowUsername'>{edituser.fullname}</span>
+        <span className='usershowPost'>{edituser.post}</span>
         <span className='usershowDepartment'>MarketingDepartment</span>
     </div>
 </div>
 <div className='userShowBottom'>
     <span className='usershowTitle'>Account Details</span>
     <div className="userShowInfo">
-    <PermIdentity className="userShowIcon"/>
-    <span className='userShowInfoTitle'>BinodKhatri</span>
+    <PermIdentity classname="userShowIcon"/>
+    <span className='userShowInfoTitle'>{edituser.fullname}</span>
     </div>
     <div className="userShowInfo">
-    <WorkOutline className="userShowIcon"/>
-    <span className='userShowInfoTitle'>Manager</span>
+    <WorkOutline classname="userShowIcon"/>
+    <span className='userShowInfoTitle'>{edituser.post}</span>
     </div>
     <div className="userShowInfo">
     <Male className="userShowIcon"/>
-    <span className='userShowInfoTitle'>Male</span>
+    <span className='userShowInfoTitle'>{edituser.gender}</span>
     </div>
-    <span className='usershowTitle'>Contact Details</span>
+    <span className='usershowTitle'>{edituser.phone}</span>
     <div className="userShowInfo">
-    <MyLocationOutlined className="userShowIcon"/>
-    <span className='userShowInfoTitle'>Bhadrapur-10</span>
-    </div>
-    <div className="userShowInfo">
-    <AlternateEmail className="userShowIcon"/>
-    <span className='userShowInfoTitle'>binodkhatri22@gmail.com</span>
+    <MyLocationOutlined classname="userShowIcon"/>
+    <span className='userShowInfoTitle'>{edituser.address}</span>
     </div>
     <div className="userShowInfo">
-    <PhoneAndroidOutlined className="userShowIcon"/>
-    <span className='userShowInfoTitle'> +977 9817637124</span>
+    <AlternateEmail classname="userShowIcon"/>
+    <span className='userShowInfoTitle'>{edituser.email}</span>
+    </div>
+    <div className="userShowInfo">
+    <PhoneAndroidOutlined classname="userShowIcon"/>
+    <span className='userShowInfoTitle'>{edituser.phone}</span>
     </div>
     
 </div>
@@ -58,37 +164,36 @@ export default function User() {
  </div>
  <div className='userUpdate'>
  <span className="userUpdateTitle">Edit</span>     
-    <form className='userUpdateForm'>
+    <form onSubmit={handleSubmit} className='userUpdateForm'>
         <div className='userUpdateLeft'>
-            <div className='userUpdateItem'>
-
-                <label>UserName</label>
-                <input 
-                type="text"
-                placeholder='binod khatri'
-                className='userUpdateInfo'
-                />
-            </div>
             <div className='userUpdateItem'>
                 <label>FullName</label>
                 <input 
+                id="fullname"
                 type="text"
+                value={edituser.fullname}
                 placeholder='binod khatri'
                 className='userUpdateInfo'
+                onChange={handleInput}
                 />
             </div>
             <div className='userUpdateItem'>
                 <label>Post</label>
-                <input 
+                <input
+                id="post"
                 type="text"
+                value={edituser.post}
                 placeholder='Manager'
+                onChange={handleInput}
                 className='userUpdateInfo'
                 />
             </div>
             <div className='userUpdateItem'>
                 <label>DepartMent</label>
                 <input 
+                id="departmnet"
                 type="text"
+                onChange={handleInput}
                 placeholder='Marketing'
                 className='userUpdateInfo'
                 />
@@ -96,7 +201,10 @@ export default function User() {
             <div className='userUpdateItem'>
                 <label>Email</label>
                 <input 
+                id="email"
                 type="text"
+                value={edituser.email}
+                onChange={handleInput}
                 placeholder='binod921@gmail.com'
                 className='userUpdateInfo'
                 />
@@ -104,7 +212,10 @@ export default function User() {
             <div className='userUpdateItem'>
                 <label>Phone</label>
                 <input 
+                id="phone"
                 type="text"
+                value={edituser.phone}
+                onChange={handleInput}
                 placeholder='+977 9817362533'
                 className='userUpdateInfo'
                 />
@@ -113,7 +224,10 @@ export default function User() {
                 <label>Address</label>
                 <input 
                 type="text"
+                id="address"
                 placeholder='Bhadrapur-10'
+                value={edituser.address}
+                onChange={handleInput}
                 className='userUpdateInfo'
                 />
             </div>
@@ -122,9 +236,9 @@ export default function User() {
      
         <div className='userUpdateRight'>
             <div className='userUpdateUpload'>
-            <img src="https://c4.wallpaperflare.com/wallpaper/260/928/20/6k-portrait-ariana-grande-wallpaper-preview.jpg" alt="" className="userUpdateImg" />
+            <img src={edituser.img} alt="profile" className="userUpdateImg" />
             <label htmlFor="file"><PublishOutlined classsName="userUpdateIcon"/></label>
-            <input type='file' id='file'style={{display:'none'}}/>
+            <input type='file' id='file'onChange={e=>setFile(e.target.files[0])}  style={{display:'none'}}/>
         </div>
         <button className='userUpdateButton'>Update</button>
 </div>
@@ -132,6 +246,8 @@ export default function User() {
 </div>
 </div>
 </div>
+{open && <Progress open={open}/>}
 </div>
+  
   )
 }
